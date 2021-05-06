bebbe0611b689891204fa74c296860430fecbc5e
MaNGuC88 <manurmamatov@gmail.com>
2021-05-06 19:23:18 +0600

    Added additional heroes

--- src/com/geektech/Main.java
+++ src/com/geektech/Main.java
@@ -4,19 +4,33 @@ import java.util.Random;
 
 public class Main {
 
-    public static String[] avengersNames = {"Captain America", "Black Panther", "Thor"};
+    public static String[] avengersNames = {"Captain America", "Black Panther", "Iron Man"};
     public static int[] avengersHealth = {220, 310, 420};
     public static int[] avengersPower = {30, 40, 50};
     public static String villainName = "Thanos";
-    public static int villainHealth = 1000;
+    public static int villainHealth = 2000;
     public static int villainPower = 100;
     public static int roundNumber = 0;
     public static String superAvenger = "";
 
     public static String medicName = "Shuri";
     public static int medicHealth = 510;
-    public static int medicHealingPower = 80;
+    public static int medicHealingPower = 100;
     public static String healedAvenger = "";
+
+    public static String golemName = "Golem";
+    public static int golemHealth = 500;
+    public static int golemPower = 10;
+
+    public static String luckyName = "Lucky";
+    public static int luckyHealth = 230;
+
+    public static String berserkName = "Berserk";
+    public static int berserkHealth = 270;
+    public static int berserkPower = 35;
+
+    public static String thorName = "Thor";
+    public static int thorHealth = 500;
 
     public static void main(String[] args) {
         printStatistics();
@@ -29,7 +43,8 @@ public class Main {
     // Round
     public static void round() {
         roundNumber++;
-        System.out.println("-----------Round Number " + roundNumber + "-----------");
+        System.out.println("-----------ROUND NUMBER " + roundNumber + "-----------");
+        thorChance();
         villainHits();
         superAvenger = getAvengerForSuperHit();
         healedAvenger = getAvengerForHealing();
@@ -48,12 +63,12 @@ public class Main {
         }
         boolean allAvengersDead = true;
         for (int avengerHealth : avengersHealth) {
-            if (avengerHealth > 0) {
+            if (avengerHealth > 0 || medicHealth > 0 || golemHealth > 0 || luckyHealth > 0 || berserkHealth > 0 || thorHealth > 0) {
                 allAvengersDead = false;
                 break;
             }
         }
-        if (allAvengersDead) {
+        if (allAvengersDead || medicHealth < 0 || golemHealth < 0 || luckyHealth < 0 || berserkHealth < 0 || thorHealth < 0) {
             System.out.println("-----------------------------------");
             System.out.println("OH NO!!! " + villainName + " WON!!! THE EARTH IS DESTROYED!!!");
             System.out.println("-----------------------------------");
@@ -65,10 +80,24 @@ public class Main {
     public static void villainHits() {
         for (int i = 0; i < avengersHealth.length; i++) {
             if (avengersHealth[i] > 0 && villainHealth > 0) {
-                avengersHealth[i] = avengersHealth[i] - villainPower;
+                avengersHealth[i] = avengersHealth[i] - (villainPower * 4/5);
             }
         }
-        medicHealth = medicHealth - villainPower;
+        if (villainHealth > 0) {
+            medicHealth = medicHealth - (villainPower * 4/5);
+        }
+        if (golemHealth > 0 && villainHealth > 0) {                                                 // 1/5 part of damage to Shuri and Thor
+            golemHealth = golemHealth - villainPower - (villainPower * avengersNames.length * 1/5) - (villainPower * 2/5);
+        }
+        if (luckyHealth > 0 && villainHealth > 0) {
+            luckyHealth = luckyHealth - villainPower * luckyChance();
+        }
+        if (berserkHealth > 0 && villainHealth > 0) {
+            berserkHealth = berserkHealth - (villainPower * 8/10);
+        }
+        if (thorHealth > 0 && villainHealth > 0) {
+            thorHealth = thorHealth - (villainPower * 4/5);
+        }
     }
 
     // Avengers Hit
@@ -87,12 +116,27 @@ public class Main {
             if (avengersHealth[i] < 0) {
                 avengersHealth[i] = 0;
             }
-            if (villainHealth < 0) {
-                villainHealth = 0;
+            if (golemHealth < 0) {
+                golemHealth = 0;
             }
-            if (medicHealth < 0) {
-                medicHealth = 0;
+            if (luckyHealth < 0) {
+                luckyHealth = 0;
             }
+            if (berserkHealth < 0) {
+                berserkHealth = 0;
+            }
+            if (thorHealth < 0) {
+                thorHealth = 0;
+            }
+        }
+        if (golemHealth > 0 && villainHealth > 0) {
+            villainHealth = villainHealth - golemPower;
+        }
+        if (berserkHealth > 0 && villainHealth > 0) {
+            villainHealth = villainHealth - berserkPower - villainPower * 2/10;
+        }
+        if (villainHealth < 0) {
+            villainHealth = 0;
         }
     }
 
@@ -106,6 +150,9 @@ public class Main {
                             + medicHealingPower + "}");
                 }
             }
+        }
+        if (medicHealth < 0) {
+            medicHealth = 0;
         }
     }
 
@@ -123,13 +170,40 @@ public class Main {
         return avengersNames[randomIndex];
     }
 
+    // Lucky's Random chance
+    public static int luckyChance() {
+        Random random = new Random();
+        int randomChance = random.nextInt(2);
+        return randomChance;
+    }
+
+    // Thor's Random chance
+    public static void thorChance(){
+        Random random = new Random();
+        int randomChance = random.nextInt(3);
+        if (randomChance == 0) {
+            villainPower = 0;
+            System.out.println("Thor stunned Thanos for one round!");
+        } else {
+            villainPower = 100;
+            System.out.println("Thor missed!");
+        }
+    }
+
     // Statistics
     public static void printStatistics() {
+        System.out.println("-----------------------------------------------");
         System.out.println(villainName + "' health = " + villainHealth + ", Damage Power = [" + villainPower + "]");
         for (int i = 0; i < avengersNames.length; i++) {
             System.out.println(avengersNames[i] + "'s health = " + avengersHealth[i] +
                     ", Damage Power = [" + avengersPower[i] + "]");
         }
-        System.out.println(medicName + "' health = " + medicHealth + ", Healing Power = [" + medicHealingPower + "]");
+        System.out.println(medicName + "'s health = " + medicHealth + ", Healing Power = [" + medicHealingPower + "]");
+        System.out.println(golemName + "'s health = " + golemHealth + ", Damage Power = [" + golemPower + "]");
+        System.out.println(luckyName + "'s health = " + luckyHealth);
+        System.out.println(berserkName + "'s health = " + berserkHealth + ", Damage Power = [" + berserkPower +
+                "], Returned Power = [" + villainPower * 2/10 + "]");
+        System.out.println(thorName + "'s health = " + thorHealth);
     }
+
 }
